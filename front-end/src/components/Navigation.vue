8a14a64d811732bb592fe3034dfce0a224c1a18f
Dany <daniel.demecko@gmail.com>
2021-06-10 12:03:36 +0000

    initial setup

---
+++ front-end/src/components/Navigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">Stats</a></li>
+      <li><a href="">Contacts</a></li>
+      <li><a href=""></a></li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Navigation extends Vue {}
+</script>
+
+
+<style scoped lang="scss">
+    ul {
+        background-color: #ccc;
+    }
+</style>
