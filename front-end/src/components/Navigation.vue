63ff92f9c7e07348f959c18fe63c502e396773fb
Dany <daniel.demecko@gmail.com>
2021-06-10 14:47:33 +0000

    route setup

--- front-end/src/components/Navigation.vue
+++ front-end/src/components/Navigation.vue
@@ -2,7 +2,7 @@
   <nav class="absolute w-full p-5 rounded bottom-0">
     <ul class="flex flex-row gap-4 justify-center">
       <li v-for="item in items" :key="item.title">
-        <router-link to="/" class="p-5 block">{{ item.title }}</router-link>
+        <router-link :to="item.route" class="p-5 block">{{ item.title }}</router-link>
       </li>
     </ul>
   </nav>
@@ -11,6 +11,7 @@
 <script lang="ts">
 import { Vue } from "vue-class-component";
 
+// TODO: IMPORT FROM ROUTER
 export default class Navigation extends Vue {
   items = [ 
     { 
@@ -23,7 +24,11 @@ export default class Navigation extends Vue {
     }, 
     { 
       title: "About", 
-      route: "/",
+      route: "/about",
+    },
+     { 
+      title: "Settings", 
+      route: "/settings",
     }
   ];
 }
--- front-end/src/views/Settings.vue
+++ front-end/src/views/Settings.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
+    <div class="p-5">
+        <h1 class="inline-block text-3xl font-extrabold text-gray-300 tracking-tight">Settings</h1>
         this is settings page
+        <form action="">
+            <select name=""> </select>
+        </form>
     </div>
 </template>
