b96c8f13e06a2f25b306d627e4d660d7e194a3d4
Dany <daniel.demecko@gmail.com>
2021-06-11 14:27:48 +0000

    added font awesome icons

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -5,4 +5,15 @@ import router from "./router";
 import store from "./store";
 import "./index.css";
 
-createApp(App).use(store).use(router).mount("#app");
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret, faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+const iconPack = [faUserSecret,faHome]
+library.add(...iconPack)
+
+
+createApp(App).use(store).use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount("#app");
